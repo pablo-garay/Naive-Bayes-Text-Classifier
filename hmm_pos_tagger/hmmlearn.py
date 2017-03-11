9aa6bb2a21bca5b827fca8d09ee7ca686f4c67b8
Pablo Garay <pgaray@roshka.com>
2017-03-11 11:35:47 -0800

    Refactored hmmlearn.py code

--- hmm_pos_tagger/hmmlearn.py
+++ hmm_pos_tagger/hmmlearn.py
@@ -6,9 +6,8 @@
 
 import json
 import unicodedata
-import sys
-
 import operator
+import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -28,42 +27,31 @@ word_dict = {}
 # Compute transition and emission probabilities from training file
 with open(f_tr_text) as tr_text:
     for line in tr_text:
-        prev_state = "q0"
-        for unit in line.strip().split(' '):
-            word = unit[:-3].lower()
+        prev_state = "q0" #initial state is "q0"
+        for token in line.strip().split(' '):
+            word = token[:-3].lower()
             # word = unicodedata.normalize('NFD', unicode(word)).encode('ascii','ignore')
-            tag = unit[-2:]
+            state = token[-2:]
             # print "word: " + word + " " + "tag: " + tag
             # possible_tags.add(tag)
+            #add word to dictionary of known words
             if word not in word_dict: word_dict[word] = 1
             else: word_dict[word] += 1
 
             # for each tag, count its number of occurrences
-            if tag not in num_tag_ocurrences:
-                num_tag_ocurrences[tag] = 1
-            else:
-                num_tag_ocurrences[tag] += 1
-
-            # for transition probabilities
-            if prev_state not in transition_prob:
-                transition_prob[prev_state] = {}
-
-            if tag not in transition_prob[prev_state]:
-                transition_prob[prev_state][tag] = 1.0
-            else:
-                transition_prob[prev_state][tag] += 1.0
-
-            prev_state = tag
+            if state not in num_tag_ocurrences: num_tag_ocurrences[state] = 1
+            else: num_tag_ocurrences[state] += 1
 
             # for emission probabilities
-            if tag not in emission_prob:
-                emission_prob[tag] = {}
+            if state not in emission_prob: emission_prob[state] = {}
+            if word not in emission_prob[state]: emission_prob[state][word] = 1.0
+            else: emission_prob[state][word] += 1.0
 
-            if word not in emission_prob[tag]:
-                emission_prob[tag][word] = 1.0
-            else:
-                emission_prob[tag][word] += 1.0
-# print transition_prob
+            # for transition probabilities
+            if prev_state not in transition_prob: transition_prob[prev_state] = {}
+            if state not in transition_prob[prev_state]: transition_prob[prev_state][state] = 1.0
+            else: transition_prob[prev_state][state] += 1.0
+            prev_state = state
 
 possible_tags = num_tag_ocurrences.keys()
 # print possible_tags
@@ -72,34 +60,21 @@ possible_tags = num_tag_ocurrences.keys()
 for prev_state in transition_prob:
     # Apply Laplace (Add-One) Smoothing
     total = sum([transition_prob[prev_state][next_state] for next_state in transition_prob[prev_state]])
-    # print total
 
-    for tag in possible_tags:
-        if tag not in transition_prob[prev_state]:
-            transition_prob[prev_state][tag] = 1.0 / float(total + len(possible_tags))
+    for state in possible_tags:
+        if state not in transition_prob[prev_state]:
+            transition_prob[prev_state][state] = 1.0 / float(total + len(possible_tags))
         else:
-            transition_prob[prev_state][tag] = (float(transition_prob[prev_state][tag]) + 1) / float(total + len(possible_tags))
-
-print transition_prob
-
-# # For debug purposes: test if probabilities sum up to 1
-# for prev_state in transition_prob:
-#     tot_sum = sum([transition_prob[prev_state][tag] for tag in transition_prob[prev_state]])
-#     print tot_sum
+            transition_prob[prev_state][state] = (float(transition_prob[prev_state][state]) + 1) / float(total + len(possible_tags))
+# print transition_prob
 
 # Calculate emission probabilities: Divide by proper denominator
-for tag in emission_prob:
-    num_ocurrences_tag = num_tag_ocurrences[tag]
-    for word in emission_prob[tag]:
-        emission_prob[tag][word] = float(emission_prob[tag][word]) / float(num_ocurrences_tag)
+for state in emission_prob:
+    num_ocurrences_tag = num_tag_ocurrences[state]
+    for word in emission_prob[state]:
+        emission_prob[state][word] = float(emission_prob[state][word]) / float(num_ocurrences_tag)
 
-# print emission_prob
-
-# # For debug purposes: test if probabilities sum up to 1
-# for tag in emission_prob:
-#     tot_sum = sum([transition_prob[tag][word] for word in transition_prob[tag]])
-#     print tot_sum
-
+# save list of known words
 included_words = 0
 with open("wordlist.txt", "wb") as f_out, open("wordcount.txt", "wb") as fc_out:
     for w, c in sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True):
@@ -112,20 +87,27 @@ with open("wordlist.txt", "wb") as f_out, open("wordcount.txt", "wb") as fc_out:
 print "total num of words: ", len(word_dict)
 print "total num of words included (not discarded): ", included_words
 
-# # For information purposes: print list of words with their count
-# with open("wordcount.txt", "wb") as f_out:
-#     for w, c in sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True):
-#         f_out.write("%s : %d\n" %(w, c))
-# print "total num of words: ", len(word_dict)
-
-
-
 # save params of model in json file
 prob = {
     "transition_prob": transition_prob,
     "emission_prob": emission_prob,
     "states": possible_tags
 }
-json_prob = json.dumps(prob)
 with open("hmmmodel.txt", 'wb') as f_hmm_params:
     json.dump(prob, f_hmm_params)
+
+
+
+
+# # For debug purposes: test if probabilities sum up to 1
+# for prev_state in transition_prob:
+#     tot_sum = sum([transition_prob[prev_state][tag] for tag in transition_prob[prev_state]])
+#     print tot_sum
+# print
+# # For debug purposes: test if probabilities sum up to 1
+# for tag in emission_prob:
+#     tot_sum = sum([transition_prob[tag][word] for word in transition_prob[tag]])
+#     print tot_sum
+
+# print transition_prob
+# print emission_prob
