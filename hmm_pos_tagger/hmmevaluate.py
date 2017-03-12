c99f49d9ea524254581993fc3eeb4bee94eadc0a
Pablo Garay <pgaray@roshka.com>
2017-03-11 23:05:38 -0800

    Added code to show tagging errors in detail

--- hmm_pos_tagger/hmmevaluate.py
+++ hmm_pos_tagger/hmmevaluate.py
@@ -13,23 +13,24 @@ with open("hmmoutput.txt", "rb") as f_predicted_tags, open("data/catalan_corpus_
     for line_predicted_tags, line_dev_tagged in izip(f_predicted_tags, f_dev_tagged):
         num_lines += 1
         tokens_predicted = line_predicted_tags.strip().split(' ')
-        tokens_predicted = [token[-2:] for token in tokens_predicted]
+        tags_predicted = [token[-2:] for token in tokens_predicted]
 
         tokens_tagged = line_dev_tagged.strip().split(' ')
-        tokens_tagged = [token[-2:] for token in tokens_tagged]
+        tags_tagged = [token[-2:] for token in tokens_tagged]
 
-        if len(tokens_predicted) != len(tokens_tagged):
+        if len(tags_predicted) != len(tags_tagged):
             print "Error: NOT SAME AMOUNT OF TAGS FOR THE SAME LINE"
             print "num_line: ", num_lines
-            print tokens_predicted
-            print tokens_tagged
+            print tags_predicted
+            print tags_tagged
             exit(1)
         else:
-            for i in range(len(tokens_predicted)):
-                if tokens_predicted[i] == tokens_tagged[i]:
+            for i in range(len(tags_predicted)):
+                if tags_predicted[i] == tags_tagged[i]:
                     correct += 1
                 else:
                     incorrect += 1
+                    print "Predicted: ", tokens_predicted[i], "\t(Correct) Should be: ", tokens_tagged[i]
 
 print "Total correct predictions: ", correct
 print "Total incorrect predictions: ", incorrect
